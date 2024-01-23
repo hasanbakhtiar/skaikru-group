d242796062edc0b6bd2b477d596659718c788434
HasanBakhtiar <hasan.doctype@gmail.com>
2024-01-23 23:29:13 +0400

    lastest

---
+++ JavaScript/lesson2-data-types/js/script.js
@@ -0,0 +1,51 @@
+// Primitive Data Types
+// var b = "Hello"; //string
+// var a = 5; //number
+// var c = true; //boolean
+
+
+// Composite Data Types 
+// var d = [5,"Hello",true];
+// var d = new Array(5,"Hello",true);
+
+// var phone = {
+//     title:"Samsung S24",
+//     price:2400
+// }
+
+
+// Special Data Types
+// var f =undefined;
+// var g = null;
+// console.log(g);
+
+
+
+// Primitive DT 
+// string
+// number
+// boolean
+
+// Composite DT
+// array
+// var info = [5, "Hello", true];
+// console.log(info);
+
+// object
+// var info = {
+//     a:"JavaScript",
+//     b:"Browser"
+// }
+// console.log(info.a);
+
+
+// Special
+// undefined
+// var info = undefined;
+// console.log(info);
+// null 
+// var info = null;
+// console.log(info);
+
+
+
---
+++ JavaScript/lesson3-operators/js/script.js
@@ -0,0 +1,112 @@
+// assignment operators
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+// var infoOne = 5;
+// var infoTwo = 15;
+// infoTwo**=infoOne; //infoTwo = infoTwo+infoOne
+// console.log(infoTwo);
+
+
+
+
+
+
+// arithmetic operators 
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+
+
+
+
+
+// comparison operators
+// ==
+// ===
+// !=
+// !==
+// < 
+// <= 
+// > 
+// >= 
+// var infoOne = "5";
+// var infoTwo = 5;
+// if (infoOne !== infoTwo) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+
+
+
+
+// logical operators
+// && - AND
+// || - OR
+// !  - NOT
+
+// if (false || false || !(10!=10) || 5<1) {
+//     console.log('true');
+// }else{
+//     console.log('false');
+// }
+
+// string operators
+// +
+// +=
+
+
+
+// var infoOne = "Javascript";
+// infoOne += " Hello";
+
+// console.log(infoOne);
+
+
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,            (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+var a = 5, b= 10;
+console.log(a,b);
+
+
+// \            (tərs slash)(Qaçındırma operatorudur)
+// var info = 'JavaScript\''+'s Book';
+// console.log(info); 
+
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+// var a = true;
+// console.log(typeof a);
+
+
+
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+var a = 5;
+
+console.log(void(a));
+
+
+
+
+
+
+
+
+    
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
