01731f13c40fd5b6b93f784c00c4ff85989cd39e
HasanBakhtiar <hasan.doctype@gmail.com>
2024-01-30 21:10:54 +0400

    lastest

---
+++ JavaScript/lesson5-loops/js/script.js
@@ -0,0 +1,70 @@
+// while
+// do while
+// for 
+
+
+
+// let a = " JavaScript" 
+
+
+// let count = 0; //variable
+
+// while(count<10){ //condition
+//     console.log(count +a); //result
+//     count++; //action
+// }
+
+
+// let count = 0;
+
+// do{ //result
+//     count++;//action
+//     console.log(count);
+// }while(count>10) //condition
+
+
+
+// const brandData = ["Oppo", "Xiaomi", "OnePlus", "ZenPhone","Samsung"];
+
+
+
+// for (let i = 0; i < brandData.length; i++) {
+//     console.log(brandData[i]);
+// }
+
+// const phonesData = [
+//     {
+//         title: "Oppo Find N2",
+//         color: "white",
+//         price: 3400,
+//     },
+//     {
+//         title: "Xiaomi 14",
+//         color: "black",
+//         price: 2300,
+//     },
+//     {
+//         title: "OnePlus 12",
+//         color: "grey",
+//         price: 2400,
+//     },
+//     {
+//         title: "ZenPhone 7",
+//         color: "pink",
+//         price: 1900,
+//     }
+// ]
+// for(let i = 0;i<phonesData.length;i++){
+//     console.log(phonesData[i].title + " - " +phonesData[i].price+"$");
+// }
+
+// ===Array=== 
+// for in 
+// for(let i in phonesData){
+//     console.log(phonesData[i].title + " - " +phonesData[i].price+"$");
+// }
+
+// for of
+// for(let data of phonesData){
+//     console.log(data.title+ " - " +data.price+"$");
+// }
---
+++ JavaScript/lesson6-array-object/js/script.js
@@ -0,0 +1,55 @@
+// const phoneBrands = ["Xiaomi","Oppo","Nubia","Oneplus"];
+// const phoneBrands = [];
+// phoneBrands[0]="Xiaomi";
+// phoneBrands[1]="Oppo";
+// phoneBrands[2]="Oneplus";
+// phoneBrands[3]="Nubia";
+
+// phoneBrands["newdata"] = "Zenphone";
+
+// const phoneBrands = ["Xiaomi",["Mi","Redmi",[9,10]],"Oppo",['Find X','Find Fold'],[1,2,3,4,[true]],"Nubia","Oneplus"];
+// console.log(phoneBrands[1][2][1]);
+// console.log(phoneBrands[3][0]);
+// console.log(phoneBrands[4][4][0]);
+
+// const phoneBrands = ["Xiaomi","Oppo","Nubia","Oneplus"];
+// phoneBrands.pop();
+// phoneBrands.shift();
+// phoneBrands.unshift('zenphone');
+// phoneBrands.push('samsung');
+// console.log(phoneBrands);
+
+
+// const phoneData = {
+//     title:"OPPO Find N2",
+//     screenSize: 6.9,
+//     ram:16,
+//     color:["black","pink","aqua"],
+//     img:"https://qonooz.com/wp-content/uploads/2022/12/oppo_find_n2.png",
+// }
+
+
+// console.log(phoneData.title);
+// console.log(phoneData.screenSize);
+
+// console.log(phoneData.color[0]);
+
+
+// const phonesData = [
+//     {
+//         title:"OPPO Find N2",
+//         screenSize: 6.9,
+//         ram:16,
+//         color:["black","pink","aqua"],
+//         img:"https://qonooz.com/wp-content/uploads/2022/12/oppo_find_n2.png",
+//     },
+//     {
+//         title:"OnePlus Open",
+//         screenSize: 7,
+//         ram:32,
+//         color:["black","white"],
+//     }
+// ]
+
+
+// console.log(phonesData[1].title);
