7568c4a7b451ec1e5ca4e82021dfadf9898d65c7
HasanBakhtiar <hasan.doctype@gmail.com>
2024-02-01 20:50:03 +0400

    lastest

---
+++ JavaScript/lesson7-functions/js/script.js
@@ -0,0 +1,79 @@
+// function name(params){
+//     code
+// }
+
+// function calculateAge(birthyear){
+//     console.log(2024-birthyear);
+// }   
+
+// calculateAge(2003);
+// calculateAge(2005);
+
+
+// function speed(km,hour){
+//     let totalSpeed = km/hour;
+//     return totalSpeed;
+// }
+
+// function turbo(){
+//     return speed(300,5) + 100;
+// }
+
+// console.log(turbo());
+
+
+
+// function info(a=10,b=20){
+//         return a+b + "Hello";
+// }
+
+// console.log(info(1,2));
+
+// console.log(info());
+
+// const info = function(){
+//     return "Hello Function"
+// }
+
+
+
+// IIFE
+// (function(a){
+//     console.log(a+"hello");
+// })(5)
+
+// Arrow function
+// ()=>{}
+
+// const info =(a)=>{
+//     return a;
+// }
+
+// console.log(info("Hello"));
+
+
+
+// const info =(test)=>{
+//     return test;
+// }
+
+// info((a)=>{
+//         console.log(a);
+// })
+
+// CallBack
+// const myInfo =(calculateAge)=>{
+//         calculateAge(2024,2003);
+// }
+// myInfo((currentTime,birthYear)=>{
+//     console.log(currentTime-birthYear);
+// })
+
+
+
+
+
+
+
+
+
---
+++ JavaScript/lesson8-methods/js/script.js
@@ -0,0 +1,23 @@
+// const phone = [
+//     {
+//         title:"Xiaomi",
+//         price:2300
+//     },
+//     {
+//         title:"Oppo",
+//         price:4300
+//     },
+//     {
+//         title:"ZenPhone",
+//         price:5400
+//     }
+// ]
+
+// phone.forEach((item,count)=>{
+//     console.log(item.title + count);
+// });
+
+
+const a = "Hello";
+const b = a.repeat(20)
+console.log(b);
